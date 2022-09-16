d44c227f36c0534b4c945487a2a40512676ea8c8
owenaraujo <owendjesus64@gmail.com>
2022-09-15 23:03:52 -0500

    error

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
   app.set("port", process.env.port || 5000)
 

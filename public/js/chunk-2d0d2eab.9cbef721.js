5f26d321d7104e41e454a042623aa560801065aa
owenaraujo <owendjesus64@gmail.com>
2022-10-02 10:40:31 -0500

    error de archivos

--- public/js/chunk-2d0d2eab.9cbef721.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d2eab"],{"5b19":function(n,e,o){"use strict";function c(n,e){return" modal "}o.r(e);const d={};d.render=c;e["default"]=d}}]);
-//# sourceMappingURL=chunk-2d0d2eab.9cbef721.js.map
